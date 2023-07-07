20b7f0e15039b76db1a8dcb61528a0c322bd2d61
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-07-07 15:35:28 +0900

    budget

--- Finmate2/resources/views/budget.blade.php
+++ Finmate2/resources/views/budget.blade.php
@@ -3,34 +3,76 @@
 @section('title', 'Budget')
 
 @section('header', 'BUDGET')
+@section('aside')
 
 @section('contents')
-    <link rel="stylesheet" href="{{ asset('/css/kja.css')  }}" >
-    <main>
-    <a href="{{ route('budgetset.get') }}" class = "budget_a">예산 수정</a>
-    <div class="monthBudget">
-        <p>{{$data['currentMonth']}}달 지출 현황 </p>
-        <p>한달 예산 : {{number_format($all)}}원</p>
-        <p>사용한 금액 : {{number_format($sumamount)}}원</p>
-    </div>
-    <br>
-    <br>
-    <p>주간 지출 현황</p>
-    <p>{{$data['startDate']}} ~ {{$data['endDate']}}</p>
-    <br>
-    <br>
-    <div class="leftone">
-        <p>남은금액</p>
-        @if($all<$sumamount)
-            <p> 남은예산이 <br> 없습니다. </p>
-        @else
-            <p>{{ $data['leftBudget']>0 ? number_format($data['leftBudget'])."원" : "예산 금액을 초과하셨습니다." }}</p>
-        @endif
-    </div>
-    <p class="weekbudget">주간예산 : {{number_format($data['weekBudget'])}}원</p>
-    <p>사용금액 : {{number_format($sumweek)}}원</p>
-    <br>
-    </main>
+<div id="content">
+    <article class="l-layout menu-detail">
+        <div class="l-inner">
+            <div class="l-title">{{$data['currentMonth']}}월 지출 현황</div>
+
+            <form id="cartListForm" name="cartListForm" method="get">
+                <input type="hidden" id="br_id" name="br_id" value="">
+                <input type="hidden" id="seq" name="seq" value="">
+                <input type="hidden" id="user_id" name="user_id" value="">
+            </form>
+
+            <input type="hidden" id="sel_item_id" value="32250">
+            <input type="hidden" id="target" value="">
+            <input type="hidden" id="sale_id" value="">
+            <section class="menu-detail__wrap">
+                <div class="half menu-detail__desc-area">
+                    <div class="l-badge-wrap">
+                        <button class="badge l-num l-hot hot1">Month<i></i></button>
+                    </div>
+                    <dl class="name">
+                        <dt class="tit" id="itemNm">한달 예산 : {{number_format($all)}}원</dt>
+
+
+                        <dd class="desc" id="description">
+                            사용한 금액 : {{number_format($sumamount)}}원
+                        </dd>
+                    </dl>
+
+                </div>
+                <div class="half menu-detail__desc-area">
+                    <div class="l-badge-wrap">
+                        <button class="badge l-num l-hot hot1">Week<i></i></button>
+                    </div>
+                    <dl class="name">
+                        <dt class="tit" id="itemNm">주간 예산 : {{number_format($data['weekBudget'])}}원</dt>
+
+
+
+                        <dd class="desc" id="description">
+                            사용한 금액 : {{number_format($sumweek)}}원
+
+                        </dd>
+                    </dl>
+
+                    <dl class="total-price">
+                        <dt class="tit">남은금액 :</dt>
+
+                        <dd class="total"><span class="l-num" id="totPrice">{{number_format($data['leftBudget'])}}</span>원</dd>
+                    </dl>
+                    
+
+                </div>
+                
+
+
+            </section>
+            <div class="result-btn" id="orderbtn" style="display: block;margin: 4rem auto;text-align: center;">
+                <a href="{{ route('budgetset.get') }}" class="budget_a"><button type="button" class="l-btn" style="border-color:#D4000B;background:#D4000B;">예산수정</button></a>
+
+
+
+            </div>
+
+        </div>
+    </article>
+</div>
+
 
 </body>
 
