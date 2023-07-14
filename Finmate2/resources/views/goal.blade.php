cc65e9c8aa94a22187fad390602b575f3023bf2b
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-07-14 11:53:50 +0900

    goal blade change

--- Finmate2/resources/views/goal.blade.php
+++ Finmate2/resources/views/goal.blade.php
@@ -6,186 +6,209 @@
 @section('contents')
 
 @include('layout.errorsvalidate')
-    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
-<h1>나의 목표</h1>
 
-<form action="{{ route('goal.post') }}" method="post" class="listbox2" id="db1">
-    @method('POST')
-    @csrf
-        <label for="title">목표</label>
-        <input type="text" class="" name="title" id="title" required placeholder="목표" >
 
-        <label for="amount">금액</label>
-        <input type="number" min="100000" max="1000000000" class="" name="amount" id="amount" required placeholder="목표금액">
 
-        <label for="startperiod">시작일자</label>
-        <input type="date" class="" name="startperiod" id="startperiod" required>
+    <article class="l-layout founded" style="width: 80vw; margin: 0 auto;">
 
-        <label for="endperiod">목표일</label>
-        <input type="date" class="" name="endperiod" id="endperiod" required >
+    <form id="db1" name="db1" action="{{ route('goal.post') }}" method="post" class="listbox2" id="db1">
 
-        <button type="button" class="button" onclick="debouncedSubmitForm()">목표 생성하기</button>
-</form>
-<br><br>
-<h2>진행 목록</h2>
 
-@php
-$num = 0;
-@endphp
+        @method('POST')
+        @csrf
+            <div class="l-inner">
+                <div class="l-title"> 나의 목표 정하기</div>
+                <!-- 고객의 소리 쓰기 영역  -->
+                <section class="notice__detail">
+                    <table class="member_table cscenter_table">
+                        <colgroup>
+                            <col width="10%" class="th_col th_col_01">
+                            <col width="40%" class="td_col th_col_02">
+                            <col width="10%" class="th_col th_col_03">
+                            <col width="40%" class="td_col th_col_04">
+                        </colgroup>
+                        <tbody>
+                            <tr>
+                                <th>목표</th>
+                                <td colspan="3">
+                                    <div class="search_form">
+                                        <input type="text" name="title" id="title" required placeholder="목표" class="input_style input_cs" onkeyup="srchBr();">
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>금액</th>
+                                <td colspan="3">
+                                    <input type="number" name="amount" id="amount" placeholder="금액" min="100000" max="1000000000" class="input_style input_cs">
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>시작일자</th>
+                                <td colspan="3">
+                                    <input type="date" name="startperiod" id="startperiod" required placeholder="목표시작일" class="input_style input_cs">
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>목표일자</th>
+                                <td colspan="3">
+                                    <input type="date" name="endperiod" id="endperiod" required placeholder="목표마감일" class="input_style input_cs">
 
-<div class="listbox1">
-    @if(isset($data))
-    <table class="table">
-        <thead>
-            <tr>
-                <th>목표</th>
-                <th>목표금액</th>
-                <th>시작일자</th>
-                <th>마감일자</th>
-                <th>진행금액</th>
-                <th>달성률</th>
-                <th>수정</th>
-                <th>삭제</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($data as $goal)
-            
-            <tr>
-                <td>{{ $goal->title }}</td>
-                <td>{{ $goal->amount }}</td>
-                <td>{{ $goal->startperiod }}</td>
-                <td>{{ $goal->endperiod }}</td>
-                <td>{{ number_format($goalint[$num]) }}</td>
-                <td>{{ ceil(($goalint[$num]/$goal->amount)*100).'%' }}</td>
-                <td>
-                    <button type="button" class="button" onclick="toggleForm({{ $goal->goalno }})">수정</button>
-                </td>
-                <td>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </section>
+                <ul class="list_info_line list_info_line_btn">
+                    <li><button type="reset" class="l-btn line" style="margin-top:0;">취소하기</button></li>
+                    <li><button type="button" class="l-btn notice-list-btn" style="margin-top:0;">등록하기</button></li>
+
+                </ul>
+            </div>
+        
+    </form>
+        <div class="l-inner">
+            <div class="l-title" style="margin-top:50px;"> 진행중인 목표들 </div>
+
+            @if(isset($data))
+
+            @foreach($data as $goal)
+
+            <!-- 고객의 소리 쓰기 영역  -->
+            <section class="notice__detail">
+                <table class="member_table cscenter_table">
+                    <colgroup>
+                        <col width="10%" class="th_col th_col_01">
+                        <col width="40%" class="td_col th_col_02">
+                        <col width="10%" class="th_col th_col_03">
+                        <col width="40%" class="td_col th_col_04">
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <th>목표</th>
+                            <td colspan="3">
+                                <div class="search_form">
+                                    <input type="text" name="title" id="title" required placeholder="{{ $goal->title }}" class="input_style input_cs" onkeyup="srchBr();">
+
+                                    
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>금액</th>
+                            <td colspan="3">
+                                <input type="number" name="amount" id="amount" placeholder="{{ $goal->amount }}" min="100000" max="1000000000" class="input_style input_cs">
+                                
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>시작일자</th>
+                            <td colspan="3">
+                                <input type="date" name="startperiod" id="startperiod" required placeholder="{{ $goal->startperiod }}" class="input_style input_cs">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>목표일자</th>
+                            <td colspan="3">
+                                <input type="date" name="endperiod" id="endperiod" required placeholder="{{ $goal->endperiod }}" class="input_style input_cs">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>달성금액</th>
+                            <td colspan="3">
+                                {{ number_format($goalint) }}
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>달성률</th>
+                            <td colspan="3">
+                                {{ ceil(($goalint/$goal->amount)*100).'%' }}
+
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+               
+
+            </section>
+            <ul class="list_info_line list_info_line_btn">
+                <li>
                     <form action="{{ route('goal.delete') }}" method="delete">
                         @csrf
                         @method('delete')
                         <input type="hidden" name="goalno" value="{{ $goal->goalno }}">
-                        <button type="submit" class="button">삭제</button>
+                        <button type="submit" class="l-btn line" style="margin-top:0;">삭제</button>
                     </form>
-                </td>
-            </tr>
-
-
-            <tr>
-                <td colspan="8">
-                    <form action="{{ route('goal.put') }}" method="put" id="form_{{ $goal->goalno }}" style="display: none;">
+                </li>
+                <li>
+                    <form action="{{ route('goal.put') }}" method="put" id="form_{{ $goal->goalno }}" >
                         @csrf
                         @method('put')
                         <input type="hidden" name="goalno" value="{{ $goal->goalno }}">
-
-                        <div class="">
-                            <label for="title">목표</label>
-                            <input type="text" class="" name="title" id="title" required placeholder="목표" value="{{ $goal->title }}">
-                        </div>
-
-                        <div class="">
-                            <label for="amount">금액</label>
-                            <input type="number" min="100000" max="1000000000" class="" name="amount" id="amount" required placeholder="목표" value="{{ $goal->amount }}">
-                        </div>
-
-                        <div class="">
-                            <label for="startperiod">시작일자</label>
-                            <input type="date" class="" name="startperiod" id="startperiod" required value="{{ $goal->startperiod }}">
-                        </div>
-
-                        <div class="">
-                            <label for="endperiod">목표일</label>
-                            <input type="date" class="" name="endperiod" id="endperiod" required value="{{ $goal->endperiod }}">
-                        </div>
-
-                        <button type="submit" class="button">수정</button>
-                        <button type="button" class="button" onclick="cancelForm({{ $goal->goalno }})">취소</button>
+                        <button type="submit" class="l-btn notice-list-btn" style="margin-top:0;">수정</button>
                     </form>
-                </td>
-            </tr>
-            @php
-            $num++;
-            @endphp
-            @endforeach
-        </tbody>
-    </table>
-    @endif
-</div>
-    <h2>달성 목록</h2>
-    @if(isset($data1))
-    <table class="table">
-        <thead>
-            <tr>
-                <th>목표</th>
-                <th>목표금액</th>
-                <th>시작일자</th>
-                <th>마감일자</th>
-                <th>삭제</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($data1 as $goal)
-            
-            <tr>
-                <td>{{ $goal->title }}</td>
-                <td>{{ $goal->amount }}</td>
-                <td>{{ $goal->startperiod }}</td>
-                <td>{{ $goal->endperiod }}</td>
-                <td>
+                </li>
+
+            </ul>
+             @endforeach
+                @endif
+        </div>
+
+        <div class="l-inner">
+            <div class="l-title" style="margin-top:50px;"> 목표 달성 목록</div>
+
+            @if(isset($data1))
+            @foreach($data1 as $goal)
+
+            <section class="notice__detail">
+                <table class="member_table cscenter_table">
+                    <colgroup>
+                        <col width="10%" class="th_col th_col_01">
+                        <col width="40%" class="td_col th_col_02">
+                        <col width="10%" class="th_col th_col_03">
+                        <col width="40%" class="td_col th_col_04">
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <th>목표</th>
+                            <td colspan="3">
+                                {{ $goal->title }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>금액</th>
+                            <td colspan="3">
+                                {{ $goal->amount }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </section>
+            <ul class="list_info_line list_info_line_btn">
+                <li>
                     <form action="{{ route('goal.delete') }}" method="delete">
                         @csrf
                         @method('delete')
                         <input type="hidden" name="goalno" value="{{ $goal->goalno }}">
-                        <button type="submit" class="button">삭제</button>
+                        <button type="submit" class="l-btn line" style="margin-top:0;">삭제</button>
+
+
                     </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @endif
-<script>
-    var debounceTimer;
+                </li>
 
-    function debounce(func, delay) {
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(func, delay);
-    }
 
-    function debouncedSubmitForm() {
-        debounce(submitForm, 1000);
-    }
+            </ul>
+        </div>
+        @endforeach
+        @endif
 
-    function submitForm() {
 
-        var form = document.getElementById('db1');
-        form.submit();
-    }
 
-    function toggleForm(goalno) {
-        var formId = 'form_' + goalno;
-        var form = document.getElementById(formId);
-        var viewId = 'view_' + goalno;
-        var view = document.getElementById(viewId);
+    </article>
 
-        if (form.style.display === 'none') {
-            form.style.display = 'block';
-            view.style.display = 'none';
-        } else {
-            form.style.display = 'none';
-        }
-    }
-
-    function cancelForm(goalno) {
-        var formId = 'form_' + goalno;
-        var form = document.getElementById(formId);
-        var viewId = 'view_' + goalno;
-        var view = document.getElementById(viewId);
-
-        form.style.display = 'none';
-        view.style.display = 'block';
-    }
-</script>
 
 @endsection
