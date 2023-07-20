133bf5afc3689938c2c6687c0e8225e6f44b1892
Choihyukjae <wo4013@naver.com>
2023-07-20 10:02:44 +0900

    0720 udt 최혁재

--- Finmate2/resources/views/transactions.blade.php
+++ Finmate2/resources/views/transactions.blade.php
@@ -104,36 +104,19 @@
             @if(isset($data))
             <div id ="search_box" style=" width:50%; text-align:center; font-size:25px; margin:0px auto; margin-top:20px; display:block; " >
                 <span style="font-size:30px; color:red; margin-bottom : 10px;">검색결과   </span><button onclick="SearchBox()">숨기기</button><br><br>
-
-                <table> 
-                <thead>
-                <tr>
-                    <th>자산명</th>
-                    <th>거래구분</th>
-                    <th>거래처</th>
-                    <th>카테고리</th>
-                    <th>거래금액</th>
-                    <th>거래일시</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($data as  $value)
-                    <tr>
-                    <td>{{ $value->assetname }}
-                    <td>@if($value->type == '0')
-                        <span>입금</span>
-                    @else
-                        <span>출금</span>
-                    @endif </td>
-                    <td>{{ $value->payee}}</td>
-                    <td>{{ $value->name }}</td>
-                    <td>{{ $value->amount}}</td>
-                    <td>{{ $value->trantime }}</td>
-                    <br>
-                    </tr>
-                @endforeach
-                </tbody>
-                </table>
+                    @foreach($data as  $value)
+                        {{ $value->assetname }}
+                            @if($value->type == '0')
+                                <span>입금</span>
+                            @else
+                                <span>출금</span>
+                            @endif
+                        {{ $value->payee }}
+                        {{ $value->name }}
+                        {{ $value->amount }}
+                        {{ $value->trantime }}
+                        <br>
+                    @endforeach
             </div>
         @endif
             
